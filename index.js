4a168ad22ec369f19b90d09cd38778ee161a18ae
ddededodediamante <lukitadiamante5@gmail.com>
2026-08-17 17:09:53 -0300

    hey

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const fs = require('fs');
-const axios = require('axios');
-const sharp = require('sharp');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+const sharp = require("sharp");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const fallbackUrl = "https://raw.githubusercontent.com/LeonardSSH/vscord/main/assets/icons/";
+const fallbackUrl =
+  "https://raw.githubusercontent.com/LeonardSSH/vscord/main/assets/icons/";
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello, world!');
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, world!");
 });
 
-app.get('/icon/:image', async (req, res) => {
-    const imageName = req.params.image;
-    const imagePath = path.join(__dirname, 'icons', `${imageName}.png`);
+app.get("/icon/:image", async (req, res) => {
+  const imageName = req.params.image;
+  const imagePath = path.join(__dirname, "icons", `${imageName}.png`);
 
-    if (fs.existsSync(imagePath)) {
-        res.set('Content-Type', 'image/png');
-        res.status(200).sendFile(imagePath);
-    } else {
-        try {
-            const fallbackImageUrl = `${fallbackUrl}${imageName}.png`;
-            const response = await axios.get(fallbackImageUrl, { responseType: 'arraybuffer' });
+  if (fs.existsSync(imagePath)) {
+    res.set("Content-Type", "image/png");
+    res.status(200).sendFile(imagePath);
+  } else {
+    try {
+      const fallbackImageUrl = `${fallbackUrl}${imageName}.png`;
+      const response = await axios.get(fallbackImageUrl, { responseType: "arraybuffer" });
 
-            const compressedImage = await sharp(response.data)
-                .resize(128, 128, { fit: 'fill' })
-                .resize(1024, 1024, { fit: 'fill' })
-                .webp({ quality: 1, alphaQuality: 0, effort: 0 })
-                .toBuffer();
+      const compressedImage = await sharp(response.data)
+        .resize(64, 64, { fit: "fill", kernel: "nearest" })
+        .resize(1024, 1024, { fit: "fill", kernel: "nearest" })
+        .jpeg({ quality: 1, mozjpeg: false })
+        .toBuffer();
 
-            res.set('Content-Type', 'image/jpeg');
-            res.status(200).send(compressedImage);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send(String(error));
-        }
+      res.set("Content-Type", "image/jpeg");
+      res.status(200).send(compressedImage);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(String(error));
     }
+  }
 });
 
 app.listen(port, () => {
-    console.log(`✅ | The server is running on port ${port}!`);
+  console.log(`✅ | The server is running on port ${port}!`);
 });
